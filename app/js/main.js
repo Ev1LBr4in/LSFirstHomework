3d936963be243e4682c2810eaf651f51dd9c9839
Garevskikh Evgeny <ivgen44@gmail.com>
2015-10-05 03:57:51 +0600

    almost done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,33 @@
 $(document).ready(function($) {
      console.log("я на главной странице");    
 });
+
+//POPUP WINDOW
+$(document).ready(function() {
+
+	$('#addproj-button').on('click', function(e) {
+		$('#addproj').bPopup({
+			modalClose: false,
+            opacity: 0.6,
+            positionStyle: 'fixed'
+		});
+	});
+
+	$('#addproj-close').on('click', function(e){
+		var bPopup = $('#addproj').bPopup();
+		bPopup.close();
+	});
+
+
+	$('#addfile-input').change(function() {
+       	var fileName = $(this).val().replace("C:\\fakepath\\", "");
+        $('#addfile-text').html(fileName);
+     }).change();
+
+
+
+
+});
+
+// FAKE FAIL INPUT
+
