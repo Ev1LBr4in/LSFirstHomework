e7d57dcfa52f81d2843e87bcfdefc057575308c6
Garevskikh Evgeny <ivgen44@gmail.com>
2015-10-05 20:44:22 +0600

    previous commit was lie, now almost done

--- app/js/main.js
+++ app/js/main.js
@@ -2,14 +2,16 @@ $(document).ready(function($) {
      console.log("я на главной странице");    
 });
 
-//POPUP WINDOW
+
 $(document).ready(function() {
 
+//POPUP WINDOW
 	$('#addproj-button').on('click', function(e) {
 		$('#addproj').bPopup({
-			modalClose: false,
-            opacity: 0.6,
-            positionStyle: 'fixed'
+	        opacity: 0.6,
+            positionStyle: 'fixed',
+            speed: 450,
+            transition: 'slideBack'
 		});
 	});
 
@@ -18,16 +20,13 @@ $(document).ready(function() {
 		bPopup.close();
 	});
 
+// FAKE INPUT
 
 	$('#addfile-input').change(function() {
-       	var fileName = $(this).val().replace("C:\\fakepath\\", "");
+       	var fileName = $(this).val().replace("C:\\fakepath\\", "").substring(0,40);
         $('#addfile-text').html(fileName);
      }).change();
 
-
-
-
 });
 
-// FAKE FAIL INPUT
 
